80315c5e90b7928a0ea9c6c3cd6e46607cfad8f3
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2023-06-11 17:12:47 +0200

    backend v2.3.2

--- ClinicaVeterinaria.API/Api/db/InitialData.cs
+++ ClinicaVeterinaria.API/Api/db/InitialData.cs
@@ -8,8 +8,13 @@ namespace ClinicaVeterinaria.API.Api.db
     {
         public static void SetData(VetRepository vets)
         {
-            var created = vets.Create(new("admin", "admin", "admin1707@gmail.com", "620797974", CipherService.Encode("admin1234"), Role.ADMIN, "Ser administrador", true));
-            created.Wait();
+            var vts = vets.FindByEmail("admin1707@gmail.com");
+            vts.Wait();
+            if (vts.Result == null)
+            {
+                var created = vets.Create(new("admin", "admin", "admin1707@gmail.com", "620797974", CipherService.Encode("admin1234"), Role.ADMIN, "Ser administrador", true));
+                created.Wait();
+            }
         }
     }
 }
