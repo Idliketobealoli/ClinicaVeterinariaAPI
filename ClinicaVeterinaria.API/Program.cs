a6e68bbbcb15cbe03a3d8cd07507b29305700b6e
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2023-06-12 14:04:57 +0200

    Update Program.cs

--- ClinicaVeterinaria.API/Program.cs
+++ ClinicaVeterinaria.API/Program.cs
@@ -89,11 +89,11 @@ namespace ClinicaVeterinaria.API
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
